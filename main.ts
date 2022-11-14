00397bdfc50716e8e1fad8d06cbbb28b21fb0ad6
Elliott-Vallee <116816433+Elliott-Vallee@users.noreply.github.com>
2022-11-14 11:56:30 -0500

    Mettre à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 basic.forever(function () {
+	
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(10000)
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -8,4 +12,10 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P2, 1)
     basic.pause(10000)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    if (input.buttonIsPressed(Button.A)) {
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(10000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
 })
